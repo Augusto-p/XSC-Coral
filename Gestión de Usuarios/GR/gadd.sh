382c05ff21e2c464e4aa26a037cead4a1da61a7d
Sr.X <82605763+kapoloman@users.noreply.github.com>
2022-07-12 13:19:41 -0300

    Correción de errores

--- Gestión de Usuarios/GR/gadd.sh
+++ Gestión de Usuarios/GR/gadd.sh
@@ -5,11 +5,11 @@ graddmanage(){
     read -p "Aquí: " gname
     echo "Ingrese el GID del grupo a crear"
     read -p "Aquí: " giden
-    sudo groupadd -g $giden $gname
     if grep -qi "$giden" /etc/group || grep -qi "$gname" /etc/group;then
         echo "El grupo que quiere crear ya existe"
         graddmanage
     else
+        sudo groupadd -g $giden $gname
         echo "El grupo fue creado exitosamente"
         echo "¿Desea continuar?"
         read -p "Si=Y No=N" con
