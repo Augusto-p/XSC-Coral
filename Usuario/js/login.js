6ea40acab62f14bfbd12ee779955b4347b2df078
Augusto Picardo <auguselo77@gmail.com>
2022-08-11 23:27:11 -0300

    Modificacion en la organisacion de los archivos, se añade las vistas sendResetPassword Y resetPassword, se remplasan los iconos .png por .svg y se modifican los sliders de la pagina principal

--- Usuario/js/login.js
+++ Usuario/js/login.js
@@ -1,10 +1,1 @@
-let inputs = document.getElementsByClassName('inputs');
-let entrar = document.getElementById('entrar');
-
-
-
-window.addEventListener('resize', setsize);
-function setsize() {
-    entrar.style.width = inputs[0].offsetWidth + 'px';
-}
 setsize();
--- Usuario/js/main.js
+++ Usuario/js/main.js
@@ -1,1 +1,17 @@
+let inputs = document.getElementsByClassName('inputs');
+let entrar = document.getElementById('entrar');
 
+
+window.addEventListener('resize', setsize);
+function setsize() {
+    entrar.style.width = inputs[0].offsetWidth + 'px';
+}
+
+function ConfirmedPasword(pass1,pass2) {
+    if (pass1 == pass2){
+        return true;
+    }else{
+        return false;
+    }
+    
+}
