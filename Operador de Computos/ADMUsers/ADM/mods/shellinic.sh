3518dd2cdb71ccf81d652678c664eeb6ed2fb546
Sr.X <82605763+kapoloman@users.noreply.github.com>
2022-09-02 13:41:00 -0300

    Comienzo del script para el operador de centro de computos y modificación menor en otros

---
+++ Operador de Computos/ADMUsers/ADM/mods/shellinic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Aplicación diseñada y creada por XSC Software Company
+shellmanage(){
+    echo "Ingrese el nombre de usuario a modificar"
+    read -p "Aquí:" username
+    if grep -qi "$username" /etc/passwd; then
+        shellproc
+    else
+        echo "El nombre de usuario no existe"
+        shellmanage
+    fi
+}
+shellproc(){
+    echo "Ingrese el nuevo Shell a utilizar"
+    read -p "Aquí:" newcapa
+    sudo usermod -s $newcapa $username
+    echo "¿Desea continuar?"
+    read -p "Si=Y No=N" con
+    if [ $con == "Y" ] || [ $con == "y" ]; then
+        source ./ADM/mod.sh
+    else
+        source ./main.sh
+    fi
+    
+}
+shellmanage
---
+++ Operador de Computos/main.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Programa hecho por XSC Software Company
+menuprin(){
+    echo "Programa orientado al operador de centro de computos"
+    echo ""
+    echo ""
+    echo ""
+    echo "Elija una de las opciones"
+    echo "1-Administración de Servicios y Procesos"
+    echo "2-Administración de Redes"
+    echo "3-Administración de Respaldos"
+    echo "4-Administración de Usuarios y Grupos"
+    echo "5-Administración de Registros"
+    echo "9-Salir"
+    read -p "Ingrese la opción aquí" opc
+    case $opc in
+        1);;
+        2);;
+        3);;
+        4)source ./ADMUsers/main.sh;;
+        5)source ./ADMLogs/main.sh;;
+        9) echo "Gracias por hacer uso de esta aplicación, tenga un buen día"
+            clear
+            exit
+        ;;
+        *)
+            echo "La opción $opc no es válida, por favor, intente nuevamente"
+            menuprin
+        ;;
+    esac
+}
+clear
+menuprin
