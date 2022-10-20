12a0399d00770614fef2c5af10171678c90d0289
Sr.X <82605763+kapoloman@users.noreply.github.com>
2022-10-20 19:41:41 -0300

    Se movieron los archivos y se modificaron lineas que llamaban a dichos archivos

--- Instalación de entorno/main.sh
+++ Instalación de entorno/main.sh
@@ -7,14 +7,14 @@ if grep -qi "xscadmin" /etc/passwd; then
 else
     echo "El usuario no pudo ser creado"
 fi
-sudo chmod 777 ./install.sh
-source ./install.sh
-sudo chmod 777 ./xa.sh
-source ./xa.sh
-sudo chmod 777 ./mysql.sh
-source ./mysql.sh
-sudo chmod 777 startup.sh
-sudo mv startup.sh /etc/init.d/startup
+sudo chmod 777 ./libs/install.sh
+source ./libs/install.sh
+sudo chmod 777 ./libs/xa.sh
+source ./libs/xa.sh
+sudo chmod 777 ./libs/mysql.sh
+source ./libs/mysql.sh
+sudo chmod 777 ./libs/startup.sh
+sudo mv ./libs/startup.sh /etc/init.d/startup
 sudo update-rc.d startup defaults
-sudo chmod 777 ./composer.sh
-source ./composer.sh
+sudo chmod 777 ./libs/composer.sh
+source ./libs/composer.sh
