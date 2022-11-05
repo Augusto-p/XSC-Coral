f12eecbed6ac5394a286ff006fa1e0d28c17fc0d
Sr.X <82605763+kapoloman@users.noreply.github.com>
2022-11-05 01:39:43 +0100

    Corrección de errores

--- Instalación de entorno/main.sh
+++ Instalación de entorno/main.sh
@@ -20,8 +20,8 @@ cd ..
 hostname -id
 echo "Ingrese la ip de este dispositivo"
 read iphst
-sudo sed -i "s/127.0.0.1/$iphst/g" "libs/backs.sh"
+sudo sed -i "s/127.0.0.1/$iphst/g" "./libs/backs.sh"
 echo "Ingrese la ip del dispositivo en el que se guardaran los respaldos de base de datos"
 read ipback
-sudo sed -i "s/xxx.xxx.x.x/$ipback/g" "libs/backs.sh"
-sudo crontab -e libs/titanomaquia
+sudo sed -i "s/xxx.xxx.x.x/$ipback/g" "./libs/backs.sh"
+sudo crontab -e ./libs/titanomaquia
