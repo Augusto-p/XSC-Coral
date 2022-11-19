629f67c3a89830e48d9e84eb99e046efe56b7236
Sr.X <82605763+kapoloman@users.noreply.github.com>
2022-11-19 02:57:01 -0300

    Corrección de errores

--- XSC-Coral/views/footer.php
+++ XSC-Coral/views/footer.php
@@ -1,40 +1,45 @@
 <footer>
-        <div class="footer-up">
-            <div class="footer-up-column">
-                <div class="footer-up-Column-in">
-                    <h3>Más información</h3>
-                    
-                    <p> MiMundo es una libreria que vende libros de buena calidad a buen precio y con contacto al soporte
-                        inmediato.
-                    
-                    </p>
-                    </div>
-                </div>
-                
-            <div class="footer-up-column">
-                <div class="footer-up-Column-in">
-                    <h3>Redes sociales</h3>
-                    <a href="https://www.facebook.com"><img src="<?php echo constant('URL'); ?>public/Recursos/icons/facebook.svg">Facebook</a>
-                    <a href="https: //twitter.com/davivlia"><img src="<?php echo constant('URL'); ?>public/Recursos/icons/twitter.svg">Twitter</a>
-                    <a href="https: //www.instagram.com/davivlia/"><img src="<?php echo constant('URL'); ?>public/Recursos/icons/instagram.svg">Instagram</a>
-                </div>
+    <div class="footer-up">
+        <div class="footer-up-column">
+            <div class="footer-up-Column-in">
+                <h3>Más información</h3>
 
-            </div>
-            <div class="footer-up-column">
-                <div class="footer-up-Column-in">
-                    <h3>Contacto</h3>
-                    <a href="mailto:xscsoftware@gmail.com"><img src="<?php echo constant('URL'); ?>public/Recursos/icons/email.svg">xscsoftware@gmail.com</a>
-                    <a href="tel:+59891234567"><img src="<?php echo constant('URL'); ?>public/Recursos/icons/phone.svg">+598 91 234 567</a>
-                </div>
-            </div>
+                <p> MiMundo es una libreria que vende libros de buena calidad a buen precio y con contacto al soporte
+                    inmediato.
 
-
-        </div>
-        <div class="footer-down">
-            <div class="copyright">
-                <h2>©2022 Todos los derechos reservados | MiMundo.</h2>
-            
+                </p>
             </div>
         </div>
 
-    </footer>
+        <div class="footer-up-column">
+            <div class="footer-up-Column-in">
+                <h3>Redes sociales</h3>
+                <a href="https://www.facebook.com/profile.php?id=100087551576206"><img
+                        src="<?php echo constant('URL'); ?>public/Recursos/icons/facebook.svg">Facebook</a>
+                <a href="https://twitter.com/davivlia"><img
+                        src="<?php echo constant('URL'); ?>public/Recursos/icons/twitter.svg">Twitter</a>
+                <a href="https://www.instagram.com/davivlia/"><img
+                        src="<?php echo constant('URL'); ?>public/Recursos/icons/instagram.svg">Instagram</a>
+            </div>
+
+        </div>
+        <div class="footer-up-column">
+            <div class="footer-up-Column-in">
+                <h3>Contacto</h3>
+                <a href="mailto:xscsoftware@gmail.com"><img
+                        src="<?php echo constant('URL'); ?>public/Recursos/icons/email.svg">xscsoftware@gmail.com</a>
+                <a href="tel:+59891234567"><img src="<?php echo constant('URL'); ?>public/Recursos/icons/phone.svg">+598
+                    91 234 567</a>
+            </div>
+        </div>
+
+
+    </div>
+    <div class="footer-down">
+        <div class="copyright">
+            <h2>©2022 Todos los derechos reservados | MiMundo.</h2>
+
+        </div>
+    </div>
+
+</footer>
